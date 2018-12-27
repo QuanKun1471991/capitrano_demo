113bc11ece0e1041caac183139adec247eeab33d
Quan <minhquan1471991@gmail.com>
2018-12-27 13:21:45 +0700

    capitrano v2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,61 +1,34 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
-
-
 # role-based syntax
 # ==================
-
 # Defines a role with one or multiple servers. The primary server in each
 # group is considered to be the first unless any hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
+set :appgodip, ENV['APPGODIP']
+set :appip, ENV['APPIP']
+set :frontendip, ENV['FRONTENDIP']
+role :app_god, %W{#{ fetch(:appgodip)}}
+role :app, %W{#{ fetch(:appip)}}
+role :web, %W{#{ fetch(:frontendip)}}
+role :db, %W{#{ fetch(:frontendip)}}
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+server ENV['APPGODIP'], user: 'guardian', roles: %w{appgodip}
+server ENV['APPIP'], user: 'guardian', roles: %w{appip}
+server ENV['FRONTENDIP'], user: 'guardian', roles: %w{web db}
 
 # Configuration
 # =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+set :rails_env,       'production'
+set :branch,          'master'
+set :stage,           'production'
+set :deploy_to,       '/opt/guardian/'
+set :puma_bind,       "unix:/tmp/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{fetch(:deploy_to)}/shared/tmp/pids/puma.state"
+set :puma_pid,        "#{fetch(:deploy_to)}/shared/tmp/pids/puma.pid"
+set :puma_access_log, "#{fetch(:deploy_to)}/shared/log/puma.access.log"
+set :puma_error_log,  "#{fetch(:deploy_to)}/shared/log/puma.error.log"
+set :linked_files,    %w{config/database.yml config/secrets.yml config/newrelic.yml}
